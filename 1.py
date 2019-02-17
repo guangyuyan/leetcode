4bdaec1a8c842e8648701e0c04dbc9a64bbb4550
yanguangyiu <ygyupc@hotmail.com>
2019-02-17 13:50:40 +0800

    leetcode 1

---
+++ 1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i = 0
+        while i < len(nums)-1:
+            j = i + 1
+            while j < len(nums):
+                if target == nums[i] + nums[j]:
+                    ls = [i,j]
+                    return ls
+                    break
+                j = j + 1
+            i = i + 1
