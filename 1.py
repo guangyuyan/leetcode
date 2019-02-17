4bfe982e8db72c9b976e7779217398ed2b68f5b6
yanguangyiu <ygyupc@hotmail.com>
2019-02-17 13:59:28 +0800

    format code add test case

--- 1.py
+++ 1.py
@@ -1,17 +1,21 @@
-class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        i = 0
-        while i < len(nums)-1:
-            j = i + 1
-            while j < len(nums):
-                if target == nums[i] + nums[j]:
-                    ls = [i,j]
-                    return ls
-                    break
-                j = j + 1
-            i = i + 1
+def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    i = 0
+    while i < len(nums) - 1:
+        j = i + 1
+        while j < len(nums):
+            if target == nums[i] + nums[j]:
+                ls = [i, j]
+                return ls
+                break
+            j = j + 1
+        i = i + 1
+
+"""
+test case:
+nums = [2, 7, 11, 15], target = 9
+"""
