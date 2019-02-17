c5501df5899814ec266e4287d7ece1cbddae5541
yanguangyiu <ygyupc@hotmail.com>
2019-02-17 14:20:09 +0800

    leetcode 1

---
+++ 1_e.py
@@ -0,0 +1,24 @@
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    i = 0
+    while i < len(nums) - 1:
+        j = i + 1
+        while j < len(nums):
+            if target == nums[i] + nums[j]:
+                ls = [i, j]
+                return ls
+                break
+            j = j + 1
+        i = i + 1
+
+
+"""
+test case:
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target))
+"""
