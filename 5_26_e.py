de571ad1635224580bd811a96386a13ad6354592
yanguangyiu <ygyupc@hotmail.com>
2019-02-22 00:57:43 +0800

    day 5

---
+++ 5_26_e.py
@@ -0,0 +1,13 @@
+def removeDuplicates(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    i = 1
+    while i < len(nums):
+        if nums[i] == nums[i - 1]:
+            nums.pop(i)
+            i -= 1
+        i += 1
+    return len(nums)
+
