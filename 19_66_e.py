1c4d90282c92d9be0400dec7096e09f8673edb20
yanguangyiu <ygyupc@hotmail.com>
2019-03-09 00:16:36 +0800

    day 16

---
+++ 19_66_e.py
@@ -0,0 +1,7 @@
+def plusOne(self, digits):
+    """
+    :type digits: List[int]
+    :rtype: List[int]
+    """
+    d = int(''.join([str(i) for i in digits])) + 1
+    return [int(i) for i in str(d)]
