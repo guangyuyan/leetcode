98733eda203d7c4030cb39cfd4abcdff44ae65f9
yanguangyiu <ygyupc@hotmail.com>
2019-03-09 00:16:48 +0800

    day 16

--- 10_104_e.py
+++ 10_104_e.py
@@ -5,4 +5,6 @@ def maxDepth(root):
     """
     if root == None:
         return 0
-    return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+    return max(maxDepth(root.left), maxDepth(root.right)) + 1
+
+print(maxDepth([3,9,20,3,2,15,7,3,4,5,6,7,8,9,0,1,2,4,5,6]))
